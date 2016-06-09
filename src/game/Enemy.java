247e4efb6ca76ca7c6484f192c69d63236b42be4
s57718 <s57718@beuth-hochschule.de>
2016-06-09 07:54:43 +0200

    Enemy Klasse bearbeitet, Konstruktor und Basis Atributte.

--- src/game/Enemy.java
+++ src/game/Enemy.java
@@ -1,7 +1,39 @@
 package game;
 
 /**
- * Created by ordencaos on 08.06.16.
+ * Created by Yahima on 08.06.16.
  */
 public class Enemy {
+
+    private int atk;
+    private int def;
+    private String mag;
+    private int lp;
+
+    Enemy(int atk, int def, String mag, int lp){
+        this.atk = atk;
+        this.def = def;
+        this.mag = mag;
+        this.lp = lp;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public String getMag() {
+        return mag;
+    }
+
+    public int getLp() {
+        return lp;
+    }
+
+    public void setLp(int lp) {
+        this.lp = lp;
+    }
 }
