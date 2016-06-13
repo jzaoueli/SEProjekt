7513d9282a59819d259032065b27b2224845906b
s57718 <s57718@beuth-hochschule.de>
2016-06-13 13:42:25 +0200

    Player Class added.

--- src/main/game/Player.java
+++ src/main/game/Player.java
@@ -4,4 +4,17 @@ package main.game;
  * Player class.
  */
 public class Player {
+    private int lifePoints;
+
+    Player(){
+
+    }
+
+    public int getLifePoints() {
+        return lifePoints;
+    }
+
+    public void setLifePoints(int lifePoints) {
+        this.lifePoints = lifePoints;
+    }
 }
