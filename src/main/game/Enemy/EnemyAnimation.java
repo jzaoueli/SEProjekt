3c26a5a31343540f6c7fedb9a16ab7bbf3e11127
s57718 <s57718@beuth-hochschule.de>
2016-06-13 13:30:31 +0200

    EnemyAnimation Class added.

---
+++ src/main/game/Enemy/EnemyAnimation.java
@@ -0,0 +1,46 @@
+package main.game.Enemy;
+
+/**
+ * Created by Pi on 10.06.2016.
+ * Class for Frame Animation
+ */
+
+import main.game.Frame;
+
+public class EnemyAnimation {
+
+    /**Counts ticks for change*/
+    private int frameCount;
+    /**Frame delay (1-12)*/
+    private int frameDelay;
+    /**Index of the current Animations Frame*/
+    private int currentFrameIndex;
+    /**Total amount of Frames in the Animation*/
+    private int totalFrames;
+    /**Frame Array*/
+    private Frame[] frames = new Frame[3];
+
+    public EnemyAnimation(Frame[] frames, int frameDelay) {
+
+        this.frames = frames;
+        this.frameDelay = frameDelay;
+        this.frameCount = 0;
+        this.currentFrameIndex = 0;
+        this.totalFrames = this.frames.length;
+    }
+
+    public void update() {
+
+        frameCount++;
+
+        if (frameCount > frameDelay) {
+            frameCount = 0;
+            currentFrameIndex++;
+            if (currentFrameIndex > totalFrames - 1) {
+                currentFrameIndex = 0;
+            } else if (currentFrameIndex < 0) {
+                currentFrameIndex = totalFrames - 1;
+            }
+        }
+    }
+}
