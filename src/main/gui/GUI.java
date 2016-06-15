fc13fd9d4622dbdcb89db089aa97b04a76ec4f6c
Yahima <norealnumber@gmail.com>
2016-06-15 02:24:22 +0200

    GUI Class implemented for Test Purposes.

--- src/main/gui/GUI.java
+++ src/main/gui/GUI.java
@@ -3,5 +3,34 @@ package main.gui;
 /**
  * Gui class
  */
-public class GUI {
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
+public class GUI extends JFrame {
+
+    public GUI() throws IOException, InterruptedException {
+        setTitle("InfiniteFight");
+        setMinimumSize(new Dimension(384, 576));
+        setMaximumSize(new Dimension(384, 576));
+        setLocationRelativeTo(null);
+        GamePanel gamePanel = new GamePanel();
+        this.add(gamePanel);
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        } );
+
+        setVisible(true);
+    }
+
+    public void update(Graphics g){
+        repaint();
+    }
 }
+
