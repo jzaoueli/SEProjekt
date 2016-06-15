5c11ef1175b13373e478ead838251cfeeab0e01c
Yahima <norealnumber@gmail.com>
2016-06-15 02:31:28 +0200

    'main' Methode of Control Class implemented for Text Purposes.

--- src/main/game/Control.java
+++ src/main/game/Control.java
@@ -1,7 +1,17 @@
 package main.game;
 
+import main.gui.GUI;
+
+import javax.swing.*;
+import java.io.IOException;
+
 /**
  * Controller class
  */
 public class Control {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        JFrame frame = new GUI();
+        frame.setVisible(true);
+    }
 }
