063bf4fe988b36795e0b706b2ced1ff656cc37aa
jzaoueli <zaoueli.jihed@gmail.com>
2016-06-26 18:34:11 +0200

    DSL: make BulletGeneratorFunction read from CSV file

--- src/dsl/antlr/function/BulletGeneratorFunction.java
+++ src/dsl/antlr/function/BulletGeneratorFunction.java
@@ -1,18 +1,29 @@
 package dsl.antlr.function;
 
 import dsl.CodeGeneratorFunction;
+import dsl.antlr.gen.GramBaseListener;
+import dsl.antlr.gen.GramLexer;
+import dsl.antlr.gen.GramParser;
+import dsl.generation.Bullet;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.io.FileReader;
 import java.io.IOException;
 
 import static dsl.CodeGeneratorFunction.getGetter;
+import static java.lang.Integer.valueOf;
 
 /**
  * Bullet class generation
  */
-public class BulletGeneratorFunction {
+public class BulletGeneratorFunction extends GramBaseListener{
+    private static Bullet bullet;
     private String content = "";
 
     public void run(String packageName) throws IOException {
+        initBullet();
         String className = "Bullet";
         CodeGeneratorFunction codeGeneratorFunction = new CodeGeneratorFunction(packageName, className);
         codeGeneratorFunction.setHeader("");
@@ -23,6 +34,34 @@ public class BulletGeneratorFunction {
         codeGeneratorFunction.setFooter();
         codeGeneratorFunction.createAndWriteInFile();
 
+    }
+
+    private static Bullet initBullet() throws IOException {
+        FileReader fileReader = new FileReader("src/dsl/antlr/src.csv");
+        ANTLRInputStream antlrInputStream = new ANTLRInputStream(fileReader);
+        // Get CSV lexer
+        GramLexer lexer = new GramLexer(antlrInputStream);
+        // Get a list of matched tokens
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // Pass the tokens to the parser
+        GramParser parser = new GramParser(tokens);
+        // Specify our entry point
+        GramParser.FileContext fileContext = parser.file();
+        // Walk it and attach our listener
+        ParseTreeWalker walker = new ParseTreeWalker();
+        GramBaseListener listener = new BulletGeneratorFunction();
+        walker.walk(listener, fileContext);
+        return bullet;
+    }
+
+    public void exitFile(GramParser.FileContext ctx) {
+        String fileName = ctx.bullet().fileName().getText();
+        int numberLine = valueOf(ctx.bullet().nubmerLine().getText());
+        int numberColumn = valueOf(ctx.bullet().numberColumn().getText());
+        int width = valueOf(ctx.bullet().objectWidth().getText());
+        int height = valueOf(ctx.bullet().objectHeight().getText());
+
+        bullet = new Bullet(fileName, numberLine, numberColumn, width, height);
     }
 
     private void setPlayerContent() {
@@ -53,10 +92,10 @@ public class BulletGeneratorFunction {
     }
 
     private String getBulletMemberVariable() {
-        return "    private String fileName = \"bullets.png\";\n" +
-                "    private int numberLine = 1;\n" +
-                "    private int numberColumn = 4;\n" +
-                "    private int width = 12;\n" +
-                "    private int height = 12;\n\n";
+        return "    private String fileName = \""+ bullet.getFileName()+"\";\n" +
+                "    private int numberLine = "+ bullet.getNumberLine()+";\n" +
+                "    private int numberColumn = "+bullet.getNumberColumn()+";\n" +
+                "    private int width = "+bullet.getWidth()+";\n" +
+                "    private int height = "+bullet.getHeight()+";\n\n";
     }
 }
