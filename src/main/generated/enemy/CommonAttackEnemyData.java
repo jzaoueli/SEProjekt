1f04d1a8b0c01708435b0e8121adfb65825b62f4
jzaoueli <zaoueli.jihed@gmail.com>
2016-07-09 23:24:50 +0200

    refactor & delete unused code in CommonAttackEnemyData to be generated

--- src/main/generated/enemy/CommonAttackEnemyData.java
+++ src/main/generated/enemy/CommonAttackEnemyData.java
@@ -5,7 +5,7 @@ import main.model.Enemy;
 /**
  * creation : 2016/06/29 13:52
  */
-public class CommonAttackEnemyData extends Enemy{
+public class CommonAttackEnemyData extends Enemy {
 
     private String fileName = "e_commonattack.png";
     private int numberLine = 3;
@@ -18,77 +18,8 @@ public class CommonAttackEnemyData extends Enemy{
     private int defence = 1;
     private int probability = 2;
 
-    private Object[] commonAttackEnemyImageData
-            = {fileName, numberLine, numberColumn, width, height,
-            movingType, speed, offense, defence, probability};
-
-    public Object[] getCommonAttackEnemyData(){
-        return commonAttackEnemyImageData;
+    public Object[] getCommonAttackEnemyData() {
+        return new Object[]{fileName, numberLine, numberColumn, width, height,
+                movingType, speed, offense, defence, probability};
     }
-
-    // TODO Klasse wird nur bis hier benutzt
-
-    /**
-     * Constructor without parameter
-     */
-    public CommonAttackEnemyData() {
-    }
-
-    /**
-     * Constructor with parameters
-     */
-    public CommonAttackEnemyData(String fileName, int numberLine, int numberColumn, int width, int height, String movingType, int speed, int offense, int defence, int probability) {
-        this.fileName = fileName;
-        this.numberLine = numberLine;
-        this.numberColumn = numberColumn;
-        this.width = width;
-        this.height = height;
-        this.movingType = movingType;
-        this.speed = speed;
-        this.offense = offense;
-        this.defence = defence;
-        this.probability = probability;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public int getNumberLine() {
-        return numberLine;
-    }
-
-    public int getNumberColumn() {
-        return numberColumn;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public String getMovingType() {
-        return movingType;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public int getOffense() {
-        return offense;
-    }
-
-    public int getDefence() {
-        return defence;
-    }
-
-    public int getProbability() {
-        return probability;
-    }
-
-
 }
