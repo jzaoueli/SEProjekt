b68fb7ec721f6305ffc4bdda16b5e2d3c6d98385
jzaoueli <zaoueli.jihed@gmail.com>
2016-07-09 16:01:50 +0200

    TT: add ImageBackGround.java and fix compilation

---
+++ src/main/generated/image/ImageBackground.java
@@ -0,0 +1,15 @@
+package main.generated.image;
+
+/**
+ * creation : 2016/07/09 15:59
+ */
+public class ImageBackground {
+
+    private String fileName = "background.jpg";
+
+    public String getFileName() {
+        return fileName;
+    }
+
+
+}
