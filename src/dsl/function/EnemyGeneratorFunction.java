1384662057f8044ada3e1a8e31341fbd44b145e4
jzaoueli <zaoueli.jihed@gmail.com>
2016-07-13 23:07:23 +0200

    DSL: refactor Enemy generation function

--- src/dsl/function/EnemyGeneratorFunction.java
+++ src/dsl/function/EnemyGeneratorFunction.java
@@ -7,51 +7,30 @@ import dsl.antlr.recognition.MyGramLexer;
 import dsl.antlr.recognition.MyGramParser;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import static java.lang.Integer.valueOf;
-import static java.util.Objects.isNull;
 
 /**
  * Enemy class generation.
  */
 public class EnemyGeneratorFunction extends MyGramBaseListener {
-    private static ArrayList<Enemy> enemyList = new ArrayList<>();
-    private static int sizeEnemy;
+
+    private Enemy enemy;
+    private String packageName;
+
     private String content = "";
+    private boolean noError = true;
 
-    public boolean run(String packageName, String src) throws IOException {
-        if (isNull(initEnemy(src))) {
-            return false;
-        }
-
-        for (int i = 0; i < sizeEnemy; i++) {
-            if (!isNull(enemyList.get(i))) {
-                String className = enemyList.get(i).getEnemyName() + "Data";
-                CodeGeneratorFunction codeGeneratorFunction = new CodeGeneratorFunction(packageName, className);
-
-                String importClass = "import main.model.Enemy;";
-                String extendsClass = "extends Enemy";
-                codeGeneratorFunction.setHeader(importClass, extendsClass);
-
-                setEnemyContent(i);
-
-                codeGeneratorFunction.setContent(content);
-                codeGeneratorFunction.setFooter();
-                codeGeneratorFunction.createAndWriteInFile();
-                System.out.println("- " + enemyList.get(i).getEnemyName() + " is generated");
-                content = "";
-            }
-        }
-
-        return true;
+    public EnemyGeneratorFunction(String packageName) {
+        this.packageName = packageName;
     }
 
-    private static ArrayList<Enemy> initEnemy(String src) throws IOException {
+    public void run(String src) throws IOException {
         FileReader fileReader = new FileReader(src);
         ANTLRInputStream antlrInputStream = new ANTLRInputStream(fileReader);
         // Get CSV lexer
@@ -64,130 +43,81 @@ public class EnemyGeneratorFunction extends MyGramBaseListener {
         MyGramParser.GramContext fileContext = parser.gram();
         // Walk it and attach our listener
         ParseTreeWalker walker = new ParseTreeWalker();
-        MyGramBaseListener listener = new EnemyGeneratorFunction();
+        MyGramBaseListener listener = new EnemyGeneratorFunction(packageName);
         walker.walk(listener, fileContext);
-        return enemyList;
     }
 
-    public void exitGram(MyGramParser.GramContext ctx) {
-        sizeEnemy = ctx.enemyCollection().enemy().size();
-        for (int index = 0; index < sizeEnemy; index++) {
-
-            if (!isNull(getEnemyData(ctx, index))) {
-                enemyList.add(getEnemyData(ctx, index));
-            } else {
-                enemyList.add(null);
-            }
-        }
+    @Override
+    public void visitErrorNode(ErrorNode node) {
+        noError = false;
     }
 
-    private Enemy getEnemyData(MyGramParser.GramContext ctx, int index) {
+    public void exitEnemy(MyGramParser.EnemyContext ctx) {
         String enemyName, fileName, movingType;
-        int numberLine, numberColumn, width, height, attack, defence, speed, probability;
+        int numberLine, numberColumn, width, height, attack, defence, speed;
 
-        if (isNull(ctx.enemyCollection().enemy(index).className().exception)) {
-            enemyName = ctx.enemyCollection().enemy(index).className().getText();
+        if (noError) {
+            enemyName = ctx.className().getText();
+            fileName = ctx.spriteObject().imageObject().fileName().getText();
+            numberLine = valueOf(ctx.spriteObject().numberLine().value().getText());
+            numberColumn = valueOf(ctx.spriteObject().numberColumn().value().getText());
+            width = valueOf(ctx.spriteObject().width().value().getText());
+            height = valueOf(ctx.spriteObject().height().value().getText());
+            movingType = ctx.movement().movingType().getText();
+            attack = valueOf(ctx.attack().value().getText());
+            defence = valueOf(ctx.defense().value().getText());
+            speed = valueOf(ctx.speed().value().getText());
         } else {
-            System.out.println("please verify name of enemy:");
-            System.out.println("        Ex: EnemyName{...}");
-            return null;
+            noError = true;
+            return;
         }
-        if (isNull(ctx.enemyCollection().enemy(index).spriteObject().imageObject().fileName().exception)) {
-            fileName = ctx.enemyCollection().enemy(index).spriteObject().imageObject().fileName().getText();
-        } else {
-            System.out.println("please verify filename of enemy:");
-            System.out.println("        Ex: file:[filename].[extension]");
-            return null;
+        enemy = new Enemy(enemyName, fileName, numberLine, numberColumn, width, height, movingType, speed, attack, defence);
+        try {
+            generateEnemy();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        if (isNull(ctx.enemyCollection().enemy(index).spriteObject().numberLine().value().exception)) {
-            numberLine = valueOf(ctx.enemyCollection().enemy(index).spriteObject().numberLine().value().getText());
-        } else {
-            System.out.println("please verify numberLine 'row' of enemy:");
-            System.out.println("        Ex: row:[int]");
-            return null;
-        }
-        if (isNull(ctx.enemyCollection().enemy(index).spriteObject().numberColumn().value().exception)) {
-            numberColumn = valueOf(ctx.enemyCollection().enemy(index).spriteObject().numberColumn().value().getText());
-        } else {
-            System.out.println("please verify numberColumn 'column' of enemy:");
-            System.out.println("        Ex: column:[int]");
-            return null;
-        }
-        if (isNull(ctx.enemyCollection().enemy(index).spriteObject().width().value().exception)) {
-            width = valueOf(ctx.enemyCollection().enemy(index).spriteObject().width().value().getText());
-        } else {
-            System.out.println("please verify width of enemy:");
-            System.out.println("        Ex: width:[int]");
-            return null;
-        }
-        if (isNull(ctx.enemyCollection().enemy(index).spriteObject().height().value().exception)) {
-            height = valueOf(ctx.enemyCollection().enemy(index).spriteObject().height().value().getText());
-        } else {
-            System.out.println("please verify height of enemy:");
-            System.out.println("        Ex: height:[int]");
-            return null;
-        }
-
-        if (isNull(ctx.enemyCollection().enemy(index).movement().movingType().exception)) {
-            movingType = ctx.enemyCollection().enemy(index).movement().movingType().getText();
-        } else {
-            System.out.println("please verify movingType of enemy:");
-            System.out.println("        Ex: movingType:[zigzag,vertical,continue]");
-            return null;
-        }
-        if (isNull(ctx.enemyCollection().enemy(index).attack().value().exception)) {
-            attack = valueOf(ctx.enemyCollection().enemy(index).attack().value().getText());
-        } else {
-            System.out.println("please verify attack of enemy :");
-            System.out.println("        Ex: attack:[int]");
-            return null;
-        }
-        if (isNull(ctx.enemyCollection().enemy(index).defense().value().exception)) {
-            defence = valueOf(ctx.enemyCollection().enemy(index).defense().value().getText());
-        } else {
-            System.out.println("please verify value defence of enemy:");
-            System.out.println("        Ex: defence:[int]");
-            return null;
-        }
-        if (isNull(ctx.enemyCollection().enemy(index).speed().value().exception)) {
-            speed = valueOf(ctx.enemyCollection().enemy(index).speed().value().getText());
-        } else {
-            System.out.println("please verify value speed of enemy :");
-            System.out.println("        Ex: speed:[int]");
-            return null;
-        }
-        if (isNull(ctx.enemyCollection().enemy(index).probability().value().exception)) {
-            probability = valueOf(ctx.enemyCollection().enemy(index).probability().value().getText());
-        } else {
-            System.out.println("please verify probability of enemy :");
-            System.out.println("        Ex: probability:[int]");
-            return null;
-        }
-        return new Enemy(enemyName, fileName, numberLine, numberColumn, width, height, movingType, speed, attack, defence, probability);
     }
 
-    private void setEnemyContent(int index) {
-        content += getEnemyMemberVariable(index);
-        content += getEnemyMethods(index);
+    private void generateEnemy() throws IOException {
+        String className = enemy.getEnemyName() + "Data";
+        CodeGeneratorFunction codeGeneratorFunction = new CodeGeneratorFunction(packageName, className);
+
+        String importClass = "import main.model.Enemy;";
+        String extendsClass = "extends Enemy";
+        codeGeneratorFunction.setHeader(importClass, extendsClass);
+
+        setEnemyContent();
+
+        codeGeneratorFunction.setContent(content);
+        codeGeneratorFunction.setFooter();
+        codeGeneratorFunction.createAndWriteInFile();
+        System.out.println("- " + enemy.getEnemyName() + " is generated");
+        content = "";
     }
 
-    private String getEnemyMethods(int index) {
-        return "    public Object[] get" + enemyList.get(index).getEnemyName() + "Data() {\n" +
+
+    private void setEnemyContent() {
+        content += getEnemyMemberVariable();
+        content += getEnemyMethods();
+    }
+
+    private String getEnemyMethods() {
+        return "    public Object[] get" + enemy.getEnemyName() + "Data() {\n" +
                 "        return new Object[]{fileName, numberLine, numberColumn, width, height,\n" +
-                "                movingType, speed, offense, defence, probability};\n" +
+                "                movingType, speed, offense, defence};\n" +
                 "    }\n";
     }
 
-    private String getEnemyMemberVariable(int index) {
-        return "    private String fileName = \"" + enemyList.get(index).getFileName() + "\";\n" +
-                "    private int numberLine = " + enemyList.get(index).getNumberLine() + ";\n" +
-                "    private int numberColumn = " + enemyList.get(index).getNumberColumn() + ";\n" +
-                "    private int width = " + enemyList.get(index).getWidth() + ";\n" +
-                "    private int height = " + enemyList.get(index).getHeight() + ";\n" +
-                "    private String movingType = \"" + enemyList.get(index).getMovingType() + "\";\n" +
-                "    private int speed = " + enemyList.get(index).getSpeed() + ";\n" +
-                "    private int offense = " + enemyList.get(index).getOffense() + ";\n" +
-                "    private int defence = " + enemyList.get(index).getDefence() + ";\n" +
-                "    private int probability = " + enemyList.get(index).getProbability() + ";\n\n";
+    private String getEnemyMemberVariable() {
+        return "    private String fileName = \"" + enemy.getFileName() + "\";\n" +
+                "    private int numberLine = " + enemy.getNumberLine() + ";\n" +
+                "    private int numberColumn = " + enemy.getNumberColumn() + ";\n" +
+                "    private int width = " + enemy.getWidth() + ";\n" +
+                "    private int height = " + enemy.getHeight() + ";\n" +
+                "    private String movingType = \"" + enemy.getMovingType() + "\";\n" +
+                "    private int speed = " + enemy.getSpeed() + ";\n" +
+                "    private int offense = " + enemy.getOffense() + ";\n" +
+                "    private int defence = " + enemy.getDefence() + ";\n\n";
     }
 }
