7df3fceaf4509dbc2a650946881bd2c8a59c8792
jzaoueli <zaoueli.jihed@gmail.com>
2016-07-14 03:49:01 +0200

    QA: test generated bullet

--- src/test/java/main/generation/GeneratedBulletTest.java
+++ src/test/java/main/generation/GeneratedBulletTest.java
@@ -1,19 +1,31 @@
 package test.java.main.generation;
 
-import org.junit.Ignore;
+import main.generated.bullet.BulletNormal;
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 /**
  * testing bullet generated class
  */
-@Ignore("TODO")
 public class GeneratedBulletTest {
 
-    @Test
-    public void testBulletNormal(){
+    private BulletNormal bullet;
+    private Object[] bulletObject;
 
+    @Before
+    public void setUp() {
+        bullet = new BulletNormal();
+    }
+
+    @Test
+    public void testBulletNormal() {
+        assertNotNull(bullet);
+        bulletObject = bullet.getBulletImage();
+        assertThat((String) bulletObject[0], CoreMatchers.containsString(".png"));
     }
 
 }
