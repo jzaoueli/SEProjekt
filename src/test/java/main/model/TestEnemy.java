b8077a7953dd8d6ebe78e325b9f82369d1085b61
jzaoueli <zaoueli.jihed@gmail.com>
2016-07-14 02:34:32 +0200

    QA: test enemy model

--- src/test/java/main/model/TestEnemy.java
+++ src/test/java/main/model/TestEnemy.java
@@ -1,12 +1,55 @@
 package test.java.main.model;
 
-import org.junit.Ignore;
+import main.generated.enemy.CommonAttackEnemyData;
+import main.model.Enemy;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
- * TODO Testing Enemy model class
+ * Testing Enemy model class
  */
-@Ignore("TODO")
 public class TestEnemy {
+
+    private Enemy enemy;
+    private Object[] enemyObject;
+
+    @Before
+    public void setUp() {
+        givenEnemyObject();
+    }
+
+    @Test
+    public void testConstructor() throws IOException {
+        assertNull(enemy);
+
+        whenCreateItem();
+        assertNotNull(enemy);
+        assertNotNull(enemy.getBoundingBox());
+
+        whenSetUpPlayer("continue",10,2);
+        assertEquals(enemy.getAttack(), 10);
+        assertEquals(enemy.getSpeed(),2);
+    }
+
+    private void whenSetUpPlayer(String movement, int attack, int speed) {
+        enemy.setMovement(movement);
+        enemy.setAttack(attack);
+        enemy.setSpeed(speed);
+    }
+
+    private void whenCreateItem() throws IOException {
+        enemy = new Enemy(enemyObject, 0);
+    }
+
+    private void givenEnemyObject() {
+        enemyObject = new CommonAttackEnemyData().getCommonAttackEnemyData();
+    }
 
 
 }
