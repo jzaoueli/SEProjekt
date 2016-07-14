b8652f9ed92e861e3e81dcd170bc513157254748
jzaoueli <zaoueli.jihed@gmail.com>
2016-07-14 02:09:09 +0200

    QA: test item model

--- src/test/java/main/model/TestItem.java
+++ src/test/java/main/model/TestItem.java
@@ -1,12 +1,40 @@
 package test.java.main.model;
 
-import org.junit.Ignore;
+import main.generated.item.HealItemData;
+import main.model.Item;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
- * TODO Testing Item model class
+ * Testing Item model class
  */
-@Ignore("TODO")
 public class TestItem {
 
+    private Item item;
 
+    @Test
+    public void testConstructor() throws IOException {
+        assertNull(item);
+
+        whenCreateItem();
+        assertNotNull(item);
+        assertNotNull(item.getBoundingBox());
+
+        whenSetUpPlayer(10);
+        assertEquals(item.getX(),10);
+    }
+
+    private void whenSetUpPlayer(int positionX) {
+        item.setX(positionX);
+    }
+
+    private void whenCreateItem() throws IOException {
+        Object[] itemObject = new HealItemData().getHealItemData();
+        item = new Item(itemObject,0,0);
+    }
 }
