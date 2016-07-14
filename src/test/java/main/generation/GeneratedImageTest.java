487b7a7c46e44034e320acaa77eef16382b7d005
jzaoueli <zaoueli.jihed@gmail.com>
2016-07-14 04:03:48 +0200

    QA: test generated image classes

--- src/test/java/main/generation/GeneratedImageTest.java
+++ src/test/java/main/generation/GeneratedImageTest.java
@@ -1,7 +1,15 @@
 package test.java.main.generation;
 
-import org.junit.Ignore;
+import main.generated.image.ImageBackground;
+import main.generated.image.ImageLogo;
+import main.generated.image.ImagePlayer;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 /**
  * test generated image classes
@@ -9,20 +17,35 @@ import org.junit.Test;
  * - ImageLogo.java
  * - ImagePlayer.java
  */
-@Ignore("TODO")
 public class GeneratedImageTest {
+    private ImagePlayer player;
+    private ImageLogo imageLogo;
+    private ImageBackground imageBackground;
+
+    @Before
+    public void setUp(){
+        player = new ImagePlayer();
+        imageLogo = new ImageLogo();
+        imageBackground = new ImageBackground();
+    }
+
     @Test
-    public void testImageBackground(){
+    public void testImageBackground() {
+        assertNotNull(imageBackground);
+        assertThat(imageBackground.getFileName(),anyOf(containsString(".png"), containsString(".jpg")));
 
     }
 
     @Test
-    public void testImageLogo(){
-
+    public void testImageLogo() {
+        assertNotNull(imageLogo);
+        assertThat(imageLogo.getFileName(), anyOf(containsString(".png"), containsString(".jpg")));
     }
 
     @Test
-    public void testImagePlayer(){
-
+    public void testImagePlayer() {
+        assertNotNull(player);
+        Object[] playerObject = player.getPlayerImage();
+        assertThat((String) playerObject[0], anyOf(containsString(".png"), containsString(".jpg")));
     }
 }
